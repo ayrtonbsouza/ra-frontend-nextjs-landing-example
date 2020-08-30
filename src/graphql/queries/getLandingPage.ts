a7d642bb4abeea6fa897d312efaa5e0d34d3f91d
Ayrton Souza <ayrtonsouza@icloud.com>
2020-08-29 23:30:51 -0300

    getting dynamic header info

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,7 +1,27 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
   query GET_LANDING_PAGE {
     landingPage {
-      logo {
+      ...logo
+      ...header
+    }
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
         alternativeText
         url
       }
